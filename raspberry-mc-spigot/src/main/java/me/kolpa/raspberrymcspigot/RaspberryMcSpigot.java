db00cc7ce57d4f2821c7a51c57c21185d721b39b
Nathan Kolpa <nathan@kolpa.me>
2020-09-29 12:20:56 +0200

    re add spigot

---
+++ raspberry-mc-spigot/src/main/java/me/kolpa/raspberrymcspigot/RaspberryMcSpigot.java
@@ -0,0 +1,20 @@
+package me.kolpa.raspberrymcspigot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RaspberryMcSpigot extends JavaPlugin
+{
+
+	@Override
+	public void onEnable()
+	{
+		// Plugin startup logic
+
+	}
+
+	@Override
+	public void onDisable()
+	{
+		// Plugin shutdown logic
+	}
+}
